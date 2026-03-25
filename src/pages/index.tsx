f1d839f37b576b350f277ce0848ebef864837de3
sahareupanshu4-afk <sahareupanshu4@gmail.com>
2026-03-26 02:15:29 +0530

    Add Pages Router files to fix HTML rendering errors

---
+++ src/pages/index.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div>
+      <h1>Cab Booking API Backend</h1>
+      <p>This is the backend API server. Use /api/* endpoints.</p>
+    </div>
+  )
+}
